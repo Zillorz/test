865fd783c59dd1aef107d13d5e9f87d6a4519edf
zillorz <78161200+Zillorz@users.noreply.github.com>
2025-01-19 20:55:40 -0500

    Add counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
+<script>
+  let num = $state(0);
+</script>
+
 <h1>Welcome to Test123</h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
+<button
+  onclick={() => {
+    num += 1;
+  }}>+</button
+>
+<span>{num}</span>
+<button
+  onclick={() => {
+    num -= 1;
+  }}>-</button
+>
