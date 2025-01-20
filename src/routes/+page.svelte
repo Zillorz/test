7be71890805f1cb23bd410230fcb04e5abc70c77
zillorz <78161200+Zillorz@users.noreply.github.com>
2025-01-19 21:02:15 -0500

    Bigger brand name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
-<h1>Welcome to Test123</h1>
+<h1>Welcome to <span class="name">Test123</span></h1>
 <p>
   Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
   documentation
 </p>
+
+<style>
+  .name {
+    color: red;
+    font-size: 1.5em;
+  }
+</style>
